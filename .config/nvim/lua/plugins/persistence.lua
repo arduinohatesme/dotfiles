7500789bf8463c6092c88c3b01051e549de31f64
arduinohatesme <awmcmillan128@gmail.com>
2026-06-22 14:06:07 -0500

    Stop automatic explorer opening in nvim

--- .config/nvim/lua/plugins/persistence.lua
+++ .config/nvim/lua/plugins/persistence.lua
@@ -1,3 +1,18 @@
+vim.api.nvim_create_autocmd("User", {
+  pattern = "PersistenceSavePre",
+  callback = function()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if vim.api.nvim_win_is_valid(win) then
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].filetype == "snacks_picker_list" then
+          vim.api.nvim_win_close(win, true)
+        end
+      end
+    end
+  end
+  }
+)
+
 return {
   "folke/persistence.nvim",
   event = "BufReadPre",
--- .config/nvim/lua/plugins/snacks-explorer.lua
+++ .config/nvim/lua/plugins/snacks-explorer.lua
@@ -2,7 +2,9 @@ return {
   desc = "Snacks File Explorer",
   recommended = true,
   "folke/snacks.nvim",
-  opts = { explorer = {} },
+  opts = { explorer = {
+    replace_netrw = false,
+  } },
   keys = {
     {
       "<leader>fe",
